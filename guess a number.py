27bee4360b60567b54e0583ef6dc61b8d4f8b008
shapudl <shapudl@users.noreply.github.com>
2014-04-29 20:45:52 +0200

    Update guess a number.py

--- guess a number.py
+++ guess a number.py
@@ -13,52 +13,59 @@ def enter(t):
     counter -= 1
     if counter == 0 : 
         print "Game over. You are out of tries."	
-        new_game(upper)
+        guess()
         return x
     else : 
         print "You still have ", counter, "guesses left!"	
 
-    x = int (t)
-    print x
-    if number < x:
-        print "Lower than ", x
-    elif number > x:
-        print "Higher than ", x
-    elif number == x:
-        print "Correct"
-        new_game(upper)
-    return x
+    if t.isdigit() :     
+        x = int (t)
+        print x
+        if number < x:
+            print "Lower than ", x
+        elif number > x:
+            print "Higher than ", x
+        elif number == x:
+            print "Correct"
+            guess()
+        return x
+    else : 
+        print "Wrong input. Please enter a number next time."
+        print "This try won't count"
+        counter +=1
+        return x
 
-def new_game(up):
+def guess():
     global upper
     global number
     global counter
 
-    upper = up    
+       
     if upper == 100 :
         counter = 7
+    
     if upper == 1000 :
         counter = 10	
     number = random.randint(1,upper)
-    print "You just started a new game in 1 - ", upper, " range"
+    print "You've just started a new game in 1 - ", upper, " range"
     print "Number you want is ", number
 
 def r1():
     global upper
     upper = 100
     print "You selected 1-100 range."
-    new_game(upper)
+    guess()
 
 def r2():
     global upper
     upper = 1000  
     print "You selected 1-1000 range"
-    new_game(upper)
+    guess()
    
 
 
 game = simplegui.create_frame("Guess The Number!", 200, 200)
-game.add_button ("Start / Restart", new_game , 100)
+game.add_button ("Start / Restart", guess , 100)
 game.add_button ("Start / Restart Range 1-100", r1, 100)
 game.add_button ("Start / Restart Range 1-1000", r2, 100)
 game.add_input("Enter a number", enter, 100)
