c1b7bdefdcf00e1db1336f948fa3a090e1c5012b
shapudl <shapudl@users.noreply.github.com>
2014-05-01 18:22:23 +0200

    Update stopwatch_game.py

--- stopwatch_game.py
+++ stopwatch_game.py
@@ -1,16 +1,24 @@
 #### work in progress
 ## stopwatch game for two players
 
+
 import simplegui
 
 # Global variables
 interval = 100
-count_try = 0
-count_win =0
+count_try_1 = 0
+count_win_1 = 0
+count_try_2 = 0
+count_win_2 = 0 
+score1 = 0
+score2 = 0
+
 count = 0
 minutes = "00"
 sec = "00"
 mili = "0"
+name1 = "Purple"
+name2 = "Yellow"
 
 # Button handlers
 def go():
@@ -23,27 +31,50 @@ def go():
     count +=1
     timer.start()
     
-def stop():
+    
+def stop():    
     global count
+    global count_try_1
+    global count_try_2
+    global count_win_1
+    global count_win_2
     
     count = 0
-    count_try = 0
-    count_win = 0
+    count_win_1 = 0
+    count_win_2 = 0
+    count_try_1 = 0
+    count_try_2 = 0
     timer.stop()
 
 def pause():
     print "wait"
     timer.stop()
     
-def play():
-    global count_try
-    global count_win
     
-    count_try += 1
+    
+def play_1():
+    global count_try_1
+    global count_win_1
+    global score1
+    
+    count_try_1 += 1
     if count % 10 == 0 :
-        count_win += 1
-   
+        count_win_1 += 1
+        score1 += 3 
+    else : 
+        score1 -= 1
+        
+def play_2():
+    global count_try_2
+    global count_win_2
+    global score2
     
+    count_try_2 += 1
+    if count % 10 == 0 :
+        count_win_2 += 1
+        score2 += 3
+    else : 
+        score2 -= 1
     
 # Helper functions    
 def format(t):
@@ -79,10 +110,13 @@ def decor(canvas):
     
     canvas.draw_line([0,35], [299,35], 1 , "White")
     canvas.draw_line([0,260], [299,260], 1 , "White")
-    canvas.draw_text("Score", [135,285], 20, "White")
-    canvas.draw_text("Purple",[50,250], 20, "Purple")
+    canvas.draw_text("Score", [137,285], 20, "White")
+    canvas.draw_text(name1 ,[50,250], 20, "Purple")
     canvas.draw_text("vs.", [150,250], 20 , "White")
-    canvas.draw_text("Yellow", [210,250], 20, "Yellow")
+    canvas.draw_text(name2, [210,250], 20, "Yellow")
+    canvas.draw_text(str(score1), [63,289], 30, "White")
+    canvas.draw_text(str(score2), [229,289], 30, "White")
+    
     
 # Define handlers        
 def draw(canvas):
@@ -90,9 +124,29 @@ def draw(canvas):
     decor(canvas)
     
     canvas.draw_text(format(count), [80,150], 50, "Maroon")
-    canvas.draw_text(score(count_win, count_try), [250, 22] , 20, "Yellow")
-    canvas.draw_text(score(count_win, count_try), [25, 22] , 20, "Purple")
+    canvas.draw_text(score(count_win_2, count_try_2), [250, 22] , 20, "Yellow")
+    canvas.draw_text(score(count_win_1, count_try_1), [25, 22] , 20, "Purple")
+    
+def purple_handler(text):
+    global name1
+    
+    name1 = text
+    return name1
 
+def yellow_handler(text):
+    global name2
+    
+    name2 = text
+    return name2 
+
+def key_handler(key1):
+    if key == simplegui.KEY_MAP['left']:
+        play_1
+    return
+
+
+
+# Register handlers
 
 frame = simplegui.create_frame("Stopwatch game", 300,300)
 timer = simplegui.create_timer(100, go )
@@ -102,10 +156,18 @@ frame.set_canvas_background("silver")
 frame.add_button("Start", go, 100)
 frame.add_button("Stop", pause , 100)
 frame.add_button("Reset", stop, 100)
-frame.add_button("Click", play, 100)
+frame.add_button("Click 1", play_1, 100)
+frame.add_button("Click 2", play_2, 100)
 
-player1 = frame.add_label("Purple player:")
-player2 = frame.add_label("Yellow player:")
+
+
+
+
+
+player1 = frame.add_input("Purple player:", purple_handler , 100)
+print str(name1)
+player2 = frame.add_input("Yellow player:", yellow_handler , 100)
+print name2
 
 frame.start()
 timer.start()
